71fd6a559ab6d4e07d3052ee2118bd54310c0cf4
DESKTOP-N8P75DI\504 <aaa>
2022-04-22 14:34:00 +0900

    dd

---
+++ 내부평가서버프로그램구현/src/main/java/서버프로그램구현/UserDao.java
@@ -0,0 +1,25 @@
+package 서버프로그램구현;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDao {
+	private static Connection con;
+	private static PreparedStatement ps;
+	private static ResultSet rs;	
+	
+	public static UserDao userDao = new UserDao ();	
+	
+	
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/javafx?serverTimezone=UTC","root","1234");
+			System.out.println("db연동성공");
+		}catch(Exception e) {
+			System.out.println("DB연동오류 사유 : "+ e);
+		}
+	}
+}
